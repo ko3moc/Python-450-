1f140fed06ebba98d75ad0f4995ec094910c6d29
Antony Jurjevich <49915351+ko3moc@users.noreply.github.com>
2020-10-22 19:32:42 +0100

    Add files via upload

---
+++ 53-4.py
@@ -0,0 +1,11 @@
+""" Napišite program koji se sastoji od dvije varijable. Vrijednosti tih
+varijabli učitajte preko tipkovnice te ih ispišite na zaslon ekrana.
+Prilikom upisivanja vrijednosti u varijable obavijestite korisnika da
+mora unijeti neku vrijednost, npr. "Unesite prvi niz znakova: ". To
+napravite bez korištenja funkcije print().  """
+
+var1 = input("Unesite prvi niz znakova: ")
+var2 = input("Unesite sljedeći niz znakova: ")
+
+print(var1, var2)
+
