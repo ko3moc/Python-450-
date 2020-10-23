c1f547920a8d0d84155bbc9a21d98010cdf83069
Antony Jurjevich <49915351+ko3moc@users.noreply.github.com>
2020-10-23 21:16:30 +0100

    Delete 53-3.py

--- EX5/53-3.py
+++
@@ -1,7 +0,0 @@
-""" Napišite program koji ispisuje niz znakova: "Hello World" te neka se
-na kraj ispisanog niza ispiše i znak "!". Uskličnik funkciji print()
-prenesite kao end argument. """
-
-var1 = "Hello world"
-
-print(var1, end = '!')
