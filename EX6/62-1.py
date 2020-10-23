eb0fbb3e7464a851554a7a22bd29a98f6948f7a4
Antony Jurjevich <49915351+ko3moc@users.noreply.github.com>
2020-10-23 21:27:04 +0100

    Delete 62-1.py

--- EX6/62-1.py
+++
@@ -1,8 +0,0 @@
-""" Napišite u programskom jeziku Python program koji najavljuje
-korištenje funkcija iz modula math te izračunajte i ispišite rezultat:
-sin(2) + cos(1) """
-
-import math 
-
-rezultat = math.sin(2) + math.cos(1)
-print(rezultat)
